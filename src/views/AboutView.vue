54e0412113564cefb8e587d49ff6b7791bc991fe
nthiti <thitiporn.ntk@gmail.com>
2024-01-08 15:18:23 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="table-main">
+    <table class="user-table">
+        <tr class="topic">
+          <th>Name</th>
+          <th>Phone</th>
+          <th>E-mail</th>
+          <th>Website</th>
+        </tr>
+        <tr>
+          <td>
+            <p v-for="user in users" :key="user.id" class="text-detail">{{user.name}}</p>
+          </td>
+          <td>
+            <p v-for="phone in users" :key="phone.id" class="text-detail">{{phone.phone}}</p>
+          </td>
+          <td>
+            <p v-for="email in users" :key="email.id" class="text-detail">{{email.email}}</p>
+          </td>
+          <td>
+            <p v-for="web in users" :key="web.id" class="text-detail">{{web.website}}</p>
+          </td>
+        </tr>
+        <!-- เขียนแบบย่อ -->
+        <tr v-for="user in users" :key="user.id" >
+          <td><p class="text-detail">{{ user.name }}</p></td>
+          <td><p class="text-detail">{{ user.phone }}</p></td>
+          <td><p class="text-detail">{{ user.email }}</p></td>
+          <td><p  class="text-detail">{{ user.website }}</p></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import userData from '@/store/users.json'
+export default {
+  data() {
+    return { users: userData}
+  }
+}
+</script>
+
+<style>
+.table-main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.topic{
+  height: 50px;
+  font-size: 20px;
+}
+.user-table td{
+  width: 250px;
+  height: 50px;
+  text-align: left;
+}
+.user-table,td,th{
+  border: 1px solid;
+  /* collapse ทำให้เส้นของตารางมีเส้นเดียว */
+  border-collapse: collapse;
+}
+.text-detail {
+  margin-left: 10px;
+}
+</style>
---
+++ src/views/CountView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="count">
+    <h1>Counter</h1>
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{counter}}</span>
+      <button @click="increaseCounter" class="btn">+</button>     
+    </div>
+    <div>
+      <li>Game</li>
+      <li>Game</li>
+      <li>Game</li>
+      <li>Game</li>
+      <li>Game</li>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0 
+    }
+  },
+  methods : {
+    increaseCounter() {
+      this.counter++
+    },
+    decreaseCounter() {
+      this.counter--
+    }
+  }
+}
+</script>
+
+<style>
+.count {
+  text-align: center;
+  padding: 20px;
+}
+.btn ,.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+.btn {
+  background: aquamarine ;
+  border-radius: 30px;
+}
+</style>
